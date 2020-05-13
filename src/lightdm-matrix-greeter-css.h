eec9721fae0831d986198474a7e1f899380f7de8
itmonksakshay <akshay.bhatt@hotmail.com>
2020-05-13 14:44:12 +0530

    added css

---
+++ src/lightdm-matrix-greeter-css.h
@@ -0,0 +1,17 @@
+/* automatically generated from matrix_lightdm_greeter.css */
+#ifdef __SUNPRO_C
+#pragma align 4 (matrix_lightdm_css)
+#endif
+#ifdef __GNUC__
+static const char matrix_lightdm_css[] __attribute__ ((__aligned__ (4))) =
+#else
+static const char matrix_lightdm_css[] =
+#endif
+{
+  " #matrix_dialog_command {\n    background-color: #262626;\n}\n #matrix_"
+  "dialog_info_label {\n    color: blue;\n    margin-top: 15px;\n    font-"
+  "size: 18px;\n}\n"
+};
+
+static const unsigned matrix_lightdm_css_length = 150u;
+
--- src/matrix_greeter.c
+++ src/matrix_greeter.c
@@ -5,62 +5,104 @@
 #include<string.h>
 #include<unistd.h>
 #include"src/lightdm-matrix-greeter-ui.h"
+#include"src/lightdm-matrix-greeter-css.h"
 
 GtkBuilder *builder;
+GtkCssProvider *css_provider;
 GtkWidget *main_window,*main_overlay,*command_dialog;
 GtkEntry *user_entry;
-GtkLabel *hostname_label,*user_entry_label,*status_response_label;
+GtkLabel *hostname_label,*user_entry_label,*status_response_label,*dialog_info_label;
 GtkEntry *user_entry_field;
-GtkButton *shutdown_button,*restart_button,*hibernate_button,*sleep_button;
+GtkButton *shutdown_button,*restart_button,*hibernate_button,*sleep_button,*dialog_command_button,*dialog_cancel_button;
 GtkImage *main_logo,*background_image;
+GtkBox *matrix_login_box,*matrix_command_box;
 GError *error = NULL;
 GMainLoop *main_loop;
 GAsyncResult *result;
 gint response;
+GdkScreen *screen;
+GdkDisplay *display;
+GdkMonitor *monitor;
+GdkRectangle geometry;
 
-    GdkScreen       *screen;
-    GdkDisplay      *display;
-    GdkMonitor      *monitor;
-    GdkRectangle     geometry;
+typedef struct dialog_data_struct {
+
+	gchar *button_name;
+	gboolean (*function_name)();
+
+}command_name;
 
 
 static LightDMGreeter *greeter;
 
 gchar *user_text = "Username",*pass_text = "Password" , *session = "i3";
 
-//Command Button Options shutdown Restart Hibernate
-static void shutdown_command_control(){
-	
-  	command_dialog = gtk_message_dialog_new(GTK_WINDOW (main_window),
-				GTK_DIALOG_MODAL| GTK_DIALOG_DESTROY_WITH_PARENT,
-				GTK_MESSAGE_OTHER,GTK_BUTTONS_NONE,
-		"Are you sure you want to close all programs and shutdown the computer?");
-           
-	gtk_dialog_add_buttons (GTK_DIALOG (command_dialog),"Return To Login", FALSE,"Shutdown", TRUE, NULL);
-	gtk_widget_show_all (command_dialog);
-  	if(gtk_dialog_run(GTK_DIALOG(command_dialog))){
-  		lightdm_shutdown(NULL);	
-	}
-  	gtk_widget_destroy(command_dialog);
+
+void center_window (GtkWindow *window){
+    GtkAllocation allocation;
+    GdkRectangle monitor_geometry;
+
+      	gdk_monitor_get_geometry(gdk_display_get_primary_monitor(gdk_display_get_default()), 
+			&monitor_geometry);    
+	gtk_widget_get_allocation (GTK_WIDGET (window), &allocation);
+    	gtk_window_move (window,
+                     monitor_geometry.x + (monitor_geometry.width - allocation.width) / 2,
+                     monitor_geometry.y + (monitor_geometry.height - allocation.height) / 2);
 }
 
-static void restart_command_control(){
-		command_dialog = gtk_message_dialog_new(GTK_WINDOW (main_window),
-				GTK_DIALOG_MODAL| GTK_DIALOG_DESTROY_WITH_PARENT,
-				GTK_MESSAGE_OTHER,GTK_BUTTONS_NONE,
-		"Are you sure you want to close all programs and Restart computer?");
-           
-	gtk_dialog_add_buttons (GTK_DIALOG (command_dialog),"Return To Login", FALSE,"Restart", TRUE, NULL);
-	gtk_widget_show_all (command_dialog);
-  	if(gtk_dialog_run(GTK_DIALOG(command_dialog))){
-		  lightdm_restart(NULL);	
+
+//Command Button Options shutdown Restart Hibernate
+static void dialog_window(command_name *data){
+	
+	
+	gtk_widget_hide(GTK_WIDGET(matrix_login_box));
+	gtk_widget_hide(GTK_WIDGET(matrix_command_box));
+	
+	command_dialog = GTK_WIDGET(gtk_builder_get_object(builder, "matrix_dialog_command"));
+	
+	dialog_command_button = GTK_BUTTON(gtk_builder_get_object(builder, 
+				"matrix_dialog_command_action_button"));
+	dialog_cancel_button = GTK_BUTTON(gtk_builder_get_object(builder, 
+				"matrix_dialog_command_button_cancel"));
+
+	gtk_button_set_label(dialog_cancel_button,"Return To Login");	
+  
+	dialog_info_label=GTK_LABEL(gtk_builder_get_object(builder,"matrix_dialog_info_label"));
+	gtk_label_set_text(dialog_info_label,
+			"Are you sure you want to close all programs and shutdown the computer?");
+	
+	gtk_button_set_label(dialog_command_button,data->button_name);
+	gtk_window_set_transient_for(GTK_WINDOW(command_dialog),GTK_WINDOW(main_window));
+	gtk_widget_show_all(command_dialog);
+  	center_window(GTK_WINDOW(command_dialog));
+	if(!gtk_dialog_run(GTK_DIALOG(command_dialog))){
+		data->function_name();	
 	}
-  	gtk_widget_destroy(command_dialog);
 
+	
+	gtk_widget_hide_on_delete(command_dialog);		
+	gtk_widget_show(GTK_WIDGET(matrix_login_box));
+	gtk_widget_show(GTK_WIDGET(matrix_command_box));
+}
 
+void construct_dialog(GtkWidget *button,gpointer user_data){
+
+ command_name *data = (command_name*)malloc(1*sizeof(command_name));
+	if(!strcmp(user_data,"restart")){	
+		data->function_name = lightdm_restart; 
+		data->button_name = "Restart";
+		dialog_window(data);
+	}else{
+		data->function_name = lightdm_shutdown; 
+		data->button_name = "Shutdown";
+		dialog_window(data);
+	}
+	g_free(data);
 }
 
 static void command_button_visiblty(){
+
+
 	if(!lightdm_get_can_suspend()){
 		gtk_widget_hide(GTK_WIDGET(sleep_button));
 
@@ -73,21 +115,16 @@ static void command_button_visiblty(){
 	}if(!lightdm_get_can_restart()){
 		gtk_widget_hide(GTK_WIDGET(restart_button));
 	}else{
-		g_signal_connect(restart_button,"clicked",G_CALLBACK(restart_command_control),NULL);
+		g_signal_connect(restart_button,"clicked",
+				G_CALLBACK(construct_dialog),"restart");
 	}if(!lightdm_get_can_shutdown()){
 		gtk_widget_hide(GTK_WIDGET(shutdown_button));
 	}else{
-		g_signal_connect(shutdown_button,"clicked",G_CALLBACK(shutdown_command_control),NULL);
+		g_signal_connect(shutdown_button,"clicked",
+				G_CALLBACK(construct_dialog),"shutdown");
 	}
 }
 
-//Show Status Label
-
-static void show_message_cb (LightDMGreeter *ldm, const gchar *text, LightDMPromptType type){
-
-  	gtk_label_set_text (status_response_label, text);
-
-}
 
 // Show Login entry prompt
 
@@ -99,6 +136,13 @@ static void show_prompt_cb (LightDMGreeter *ldm, const gchar *text, LightDMPromp
 
 }
 
+gboolean is_user(const gchar *username){
+	if(lightdm_user_list_get_user_by_name(lightdm_user_list_get_instance(),username) != NULL
+			){
+		return TRUE;
+	}
+	return FALSE;
+}
 
 static void login_cb() {
 	gtk_label_set_text(status_response_label,"");
@@ -108,31 +152,41 @@ static void login_cb() {
     	} else if (lightdm_greeter_get_in_authentication(greeter)) {
       	    	// authentication in progress - send password
         	lightdm_greeter_respond(greeter,gtk_entry_get_text(user_entry_field),NULL);
-    	} else {
-	          // authentication request - send username
+    	} else if(!is_user(gtk_entry_get_text(user_entry_field))){
+        	gtk_label_set_text(status_response_label, "Invalid User");
+		gtk_entry_set_text(user_entry_field, "");
+
+	} else {
+		// authentication request - send username
         	lightdm_greeter_authenticate(greeter,gtk_entry_get_text(user_entry_field),NULL);
-    	}
+	}
 }
 
 // Authentication Response
 
 static void authentication_complete_cb (LightDMGreeter *ldm) {
 
-    if (!lightdm_greeter_get_is_authenticated (ldm)) {
-        gtk_label_set_text(status_response_label, "Authentication Failure.");
+	if (!lightdm_greeter_get_is_authenticated (ldm)) {
+        	gtk_label_set_text(status_response_label, "Authentication Failure.");
 
     } else if (!lightdm_greeter_start_session_sync (ldm,session, NULL)) {
-        gtk_label_set_text(status_response_label, "Failed to start session.");
+       	gtk_label_set_text(status_response_label, "Failed to start session.");
 
     }
-
-    lightdm_greeter_authenticate (ldm, NULL, NULL);
-
+  	    lightdm_greeter_authenticate (ldm, NULL, NULL);
 }
 
 int main(int argc,char **argv){
 		
 	gtk_init(&argc,&argv);
+
+	css_provider = gtk_css_provider_new ();
+    	gtk_css_provider_load_from_data (css_provider,matrix_lightdm_css,
+			matrix_lightdm_css_length, NULL);
+    	gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), 
+			GTK_STYLE_PROVIDER(css_provider),
+                        GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
 
 	builder = gtk_builder_new ();
 	if(!gtk_builder_add_from_string(builder,lightdm_gtk_greeter_ui,
@@ -142,12 +196,18 @@ int main(int argc,char **argv){
 	}
 	g_clear_error(&error);
 
+
+
 	main_window = GTK_WIDGET(gtk_builder_get_object(builder, "matrix_greeter_window"));
 	main_overlay = GTK_WIDGET(gtk_builder_get_object(builder,"matrix_greeter_overlay"));
 	main_logo = GTK_IMAGE(gtk_builder_get_object(builder,"matrix_header_logo"));
 	
+
 	hostname_label = GTK_LABEL(gtk_builder_get_object(builder,"matrix_hostname_label"));
 	
+	matrix_login_box =  GTK_BOX(gtk_builder_get_object(builder,"matrix_login_box"));
+	matrix_command_box = GTK_BOX(gtk_builder_get_object(builder,"matrix_command_box"));
+
 	user_entry_label = GTK_LABEL(gtk_builder_get_object(builder,"matrix_input_label"));
 	gtk_label_set_text(user_entry_label,user_text);
 
@@ -160,16 +220,14 @@ int main(int argc,char **argv){
 	hibernate_button = GTK_BUTTON(gtk_builder_get_object(builder,"matrix_hibernate_button"));
 	sleep_button = GTK_BUTTON(gtk_builder_get_object(builder,"matrix_sleep_button"));
 
-
 	command_button_visiblty();
-    	gdk_monitor_get_geometry(gdk_display_get_primary_monitor(gdk_display_get_default()), &geometry);
+    	gdk_monitor_get_geometry(gdk_display_get_primary_monitor(gdk_display_get_default()), 
+			&geometry);
     	gtk_window_set_default_size(GTK_WINDOW(main_window),geometry.width,geometry.height);
 	
 	gtk_widget_show(GTK_WIDGET(main_window));
 
-
 	g_signal_connect(user_entry_field,"activate",G_CALLBACK(login_cb), NULL);
-
 
 	gtk_label_set_text(hostname_label,lightdm_get_hostname());
 
@@ -177,16 +235,13 @@ int main(int argc,char **argv){
 		
 	greeter = lightdm_greeter_new();
 	g_signal_connect (greeter, "show-prompt", G_CALLBACK (show_prompt_cb), NULL);
-	g_signal_connect (greeter, "show-message", G_CALLBACK (show_message_cb), NULL);
     	g_signal_connect (greeter, "authentication-complete",
 		       	G_CALLBACK (authentication_complete_cb), NULL);
 	
 
-	 if(!lightdm_greeter_connect_to_daemon_sync(greeter,&error)){
-	   return EXIT_FAILURE;		
+	if(!lightdm_greeter_connect_to_daemon_sync(greeter,NULL)){
+	 	return EXIT_FAILURE;
 	}
-		
-	lightdm_greeter_authenticate(greeter, NULL, NULL);
 
 	main_loop = g_main_loop_new (NULL, 0);
 
